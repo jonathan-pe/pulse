c83c2669011199470c03c9a0541580b493618cf4
Jonathan Pe <jonathanqpe@gmail.com>
2024-11-13 21:46:20 -0800

    Remove Supabase environment variables from configuration and update example file with React editor setting

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,6 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   /* config options here */
   env: {
-    SUPABASE_PUBLIC_URL: process.env.SUPABASE_PUBLIC_URL,
-    SUPABASE_PUBLIC_ANON_KEY: process.env.SUPABASE_PUBLIC_ANON_KEY,
     AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
     BACKEND_URL: process.env.BACKEND_URL,
     HCAPTCHA_SITE_KEY: process.env.HCAPTCHA_SITE_KEY,
