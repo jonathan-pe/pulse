b63353f5f1d6672156e17247225126dc2a8d72d2
Jonathan Pe <jonathanqpe@gmail.com>
2025-12-01 15:55:58 -0800

    fix: simplify AccountMenu layout in AppSidebar for cleaner presentation

--- apps/web/src/components/layout/AppSidebar.tsx
+++ apps/web/src/components/layout/AppSidebar.tsx
@@ -153,9 +153,7 @@ export default function AppSidebar() {
 
             {/* When expanded: show ThemeToggle + AccountMenu */}
             <div className='flex flex-1 items-center gap-2 group-data-[collapsible=icon]:hidden justify-between'>
-              <div className='flex-1'>
-                <AccountMenu />
-              </div>
+              <AccountMenu />
               <ThemeToggle />
             </div>
           </SidebarMenuItem>
